AOKP/packages_services_Telephony

(3/3) Contact details on incoming call
Change-Id: I932714de440df471371f08f2b60be7d2bcbab2cf
Signed-off-by: kecinzer <kecinzer@gmail.com>

src/com/android/phone/CallFeaturesSetting.java
@@ -211,6 +211,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_CHOOSE_REVERSE_LOOKUP_PROVIDER =
             "button_choose_reverse_lookup_provider";
     private static final String BUTTON_SMART_PHONE_CALL_KEY = "button_smart_phone_call";
+    private static final String BUTTON_DETAILED_INCALL_INFO_KEY = "button_detailed_incall_info";
 
     private static final String FLIP_ACTION_KEY = "flip_action";
 
@@ -309,6 +310,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private ListPreference mFlipAction;
 
     private CheckBoxPreference mSmartCall;
+    private CheckBoxPreference mDetailedIncallInfo;
 
     private class VoiceMailProvider {
         public VoiceMailProvider(String name, Intent intent) {
@@ -571,6 +573,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mSmartCall){
             Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.SMART_PHONE_CALLER,
                     mSmartCall.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mDetailedIncallInfo){
+            Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.DETAILED_INCALL_INFO,
+                    mDetailedIncallInfo.isChecked() ? 1 : 0);
             return true;
         }
         return false;
@@ -1727,6 +1733,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
         mSmartCall.setChecked(Settings.AOKP.getInt(getContentResolver(),
                 Settings.AOKP.SMART_PHONE_CALLER, 0) != 0 ? true : false);
+
+        mDetailedIncallInfo = (CheckBoxPreference) findPreference(BUTTON_DETAILED_INCALL_INFO_KEY);
+        mDetailedIncallInfo.setChecked(Settings.AOKP.getInt(getContentResolver(),
+                Settings.AOKP.DETAILED_INCALL_INFO, 0) != 0 ? true : false);
 
         // create intent to bring up contact list
         mContactListIntent = new Intent(Intent.ACTION_GET_CONTENT);

CM11MOD/android_packages_services_Telephony

Conflicts:
	res/values-cs/strings_custom.xml
	res/values/strings_custom.xml
	res/xml/call_feature_setting.xml
	src/com/android/phone/CallFeaturesSetting.java

src/com/android/phone/CallFeaturesSetting.java
@@ -195,8 +195,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_GSM_UMTS_OPTIONS = "button_gsm_more_expand_key";
     private static final String BUTTON_CDMA_OPTIONS = "button_cdma_more_expand_key";
 
+    private static final String BUTTON_DETAILED_INCALL_INFO_KEY = "button_detailed_incall_info";
     private static final String INCALL_GLOWPAD_TRANSPARENCY = "incall_glowpad_transparency";
-
     private static final String DIALKEY_PADDING = "dialkey_padding";
 
     private static final String VM_NUMBERS_SHARED_PREFERENCES_NAME = "vm_numbers";
@@ -312,6 +312,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private CheckBoxPreference mPlayDtmfTone;
     private CheckBoxPreference mButtonAutoRetry;
     private CheckBoxPreference mButtonHAC;
+    private CheckBoxPreference mDetailedIncallInfo;
     private CheckBoxPreference mIncallGlowpadTransparency;
     private ListPreference mDialkeyPadding;
     private ListPreference mButtonDTMF;
@@ -606,6 +607,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mSmartCall){
 			Settings.System.putInt(getContentResolver(), Settings.System.SMART_PHONE_CALLER,
             mSmartCall.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mSmartCall){
+            Settings.System.putInt(getContentResolver(), Settings.System.SMART_PHONE_CALLER,
+                    mSmartCall.isChecked() ? 1 : 0);
             return true;
         } else if (preference == mCallEndSound){
             Settings.System.putInt(getContentResolver(), Settings.System.CALL_END_SOUND,
@@ -1841,6 +1846,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
         mSmartCall.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.SMART_PHONE_CALLER, 0) != 0 ? true : false);
+
+        mDetailedIncallInfo = (CheckBoxPreference) findPreference(BUTTON_DETAILED_INCALL_INFO_KEY);
+        mDetailedIncallInfo.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DETAILED_INCALL_INFO, 0) != 0 ? true : false);
 
         mCallEndSound = (CheckBoxPreference) findPreference(BUTTON_CALL_END_SOUND_KEY);
         mCallEndSound.setChecked(Settings.System.getInt(getContentResolver(),